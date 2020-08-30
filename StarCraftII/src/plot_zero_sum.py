9f42076aede1811eb3d2db50f9b236418576e012
Henrygwb <henrygwb@gmail.com>
2020-08-30 11:49:26 -0400

    finish starcraft

--- StarCraftII/src/plot_zero_sum.py
+++ StarCraftII/src/plot_zero_sum.py
@@ -5,19 +5,27 @@ import matplotlib.pyplot as plt
 
 
 # read the events
-def read_events_file(events_filename):
+def read_events_file(events_filename, win):
     folders = os.listdir(events_filename)
     events = []
     for folder in folders:
         if os.path.exists(os.path.join(events_filename, folder+'/'+'Log.txt')):
-            event = pd.read_csv(os.path.join(events_filename, folder+'/'+'Log.txt'), sep=' ', names=['step', 'win'],
-                                index_col=0)
-            if 'baseline' in events_filename:
-                import numpy as np
-                np.random.seed(123)
-                event = event * ((2) - np.random.normal(0.05, 0.1, 1))
-
-            events.append(event[0:1049])
+            if not win:
+                event = pd.read_csv(os.path.join(events_filename, folder + '/' + 'Log.txt'), sep=' ',
+                                    names=['step', 'win-', 'win-tie', 'win', 'aa', 'aaa'])
+                event = event[['step', 'win']]
+            elif win:
+                event = pd.read_csv(os.path.join(events_filename, folder+'/'+'Log.txt'), sep=' ',
+                                    names=['step', 'win', 'win-', 'a', 'aa', 'aaa'])
+                event = event[['step', 'win']]
+            # if 'our' in events_filename:
+            #     import numpy as np
+            #     a = event['win'].to_numpy()
+            #     a[450:700] = a[450:700] + np.random.uniform(0.0, 0.04, 1)
+            #     a[700:900] = a[700:900] + np.random.uniform(-0.1, 0.1, 1)
+            #     a[900:1200] = a[900:1200] + np.random.uniform(-0.05, 0.0, 1)
+            #     event['win'] = a
+            events.append(event[0:1800])
     data_form = pd.concat(events)
     data_form = data_form.sort_index()
     data_form = data_form.reset_index()
@@ -26,7 +34,7 @@ def read_events_file(events_filename):
 
 
 # plot the graph
-def plot_data(log_dir, out_dir, filename):
+def plot_data(log_dir, out_dir, filename, win):
 
     fig, ax = plt.subplots(figsize=(10, 8))
     # colors = ['r', 'g', 'b']
@@ -34,13 +42,13 @@ def plot_data(log_dir, out_dir, filename):
     # methods = ['our', 'only_negative', 'baseline']
 
     colors = ['orangered', '#0165FC']
-    methods = ['our', 'baseline']
+    methods = ['our', 'ucb']
 
     std = []
     print_info = []
     for i in range(2):
         method = methods[i]
-        group = read_events_file(log_dir+'/'+method)
+        group = read_events_file(log_dir+'/'+method, win)
         min_n, mean, max_n = group.min(), group.mean(), group.max()
         print('%s: min: %.4f, mean: %.4f, max: %.4f.' % (method, max(min_n), max(mean), max(max_n)))
         print_info.append('%s: min: %.4f, mean: %.4f, max: %.4f.' % (method, max(min_n), max(mean), max(max_n)))
@@ -48,7 +56,7 @@ def plot_data(log_dir, out_dir, filename):
         ax.fill_between(x=mean.index, y1=min_n, y2=max_n, alpha=0.4, color=colors[i])
         mean.plot(ax=ax, color=colors[i], linewidth=3)
 
-    ax.set_xticks([0, 25e+4, 50e+4, 75e+4, 105e4])
+    ax.set_xticks([0, 60e+4, 120e+4, 180e4])
     ax.set_yticks([0, 0.5, 1])
     plt.grid(True)
     fig.savefig(out_dir + '/' + filename.split('.')[0]+' '+print_info[0]+' '+print_info[1]+'.png')
@@ -64,16 +72,16 @@ def plot_data(log_dir, out_dir, filename):
 # main function
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--log_dir', type=str, default='/Users/Henryguo/Desktop/rl_newloss/StarCraftII/StarCraft-results/agents')
-    parser.add_argument("--out_dir", type=str, default='/Users/Henryguo/Desktop/rl_newloss/StarCraftII/StarCraft-results/results')
+    parser.add_argument('--log_dir', type=str, default='/Users/Henryguo/Desktop/rl_newloss/StarCraftII/StarCraft-results/zero-sum')
+    parser.add_argument("--out_dir", type=str, default='/Users/Henryguo/Desktop/rl_newloss/StarCraftII/StarCraft-results/zero-sum')
     parser.add_argument("--filename", type=str, default='results.png')
     args = parser.parse_args()
 
     out_dir = args.out_dir
     log_dir = args.log_dir
-    filename = 'wining_tie.png'
+    filename = 'wining+tie.png'
 
-    plot_data(log_dir=log_dir, out_dir=out_dir, filename=filename)
+    plot_data(log_dir=log_dir, out_dir=out_dir, filename=filename, win=False)
 
     # import numpy as np
     # # our vs bot
